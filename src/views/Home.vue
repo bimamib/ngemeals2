5a438aeb73f563d24a31f619ceaabbf8c7f1d14a
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-11-23 22:46:45 +0700

    Feat: Loading Progres in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="flex flex-col p-8 pb-0 text-red-600">
     <h1 class="mb-4 text-4xl font-bold">Random Meals</h1>
   </div>
-  <Meals :meals="meals" />
+  <Loading :visible="loading" />
+  <Meals v-if="!loading" :meals="meals" />
 </template>
 
 <script setup>
@@ -10,8 +11,10 @@ import { computed, onMounted, ref } from "vue";
 import store from "../store";
 import axiosClient from "../axiosClient";
 import Meals from "../components/Meals.vue";
+import Loading from "../components/Loading.vue";
 
 const meals = ref([]);
+const loading = ref(true);
 
 onMounted(async () => {
   for (let i = 0; i < 10; i++) {
@@ -23,5 +26,6 @@ onMounted(async () => {
       console.error(`Error fetching meal ${i + 1}:`, error.message);
     }
   }
+  loading.value = false;
 });
 </script>
